f08c2660dbba59c6998257f906acd8648d40adce
Adrian Bobak <abobak@szkla.com>
2019-01-11 08:48:48 +0100

    Dodatnie font-awesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 // Add global scss file
 require('../css/global.scss');
 
+// Add font-awesome
+require('@fortawesome/fontawesome-free/js/all.js');
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
 require('../css/banner.css');
