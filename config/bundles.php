5093dcbdbd31bf474e7a9d74e1f5d184ba1fa10d
Adrian Bobak <abobak@szkla.com>
2019-07-25 13:53:33 +0200

    Dodanie zabezpieczenia dla admina.

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
